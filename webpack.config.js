079c13c708c3d9bd0d1acaa6e5411f34f3f3586c
hrkd <hiroki@hrkd.net>
2019-09-26 17:26:54 +0900

    use webpack/ts

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts']
+  }
+};
